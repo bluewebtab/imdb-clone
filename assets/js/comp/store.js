5ab723b68429b75da6614866d674129550647bcb
Ronny <ronny.icne@gmail.com>
2018-12-13 15:44:52 -0800

    update store file

--- assets/js/comp/store.js
+++ assets/js/comp/store.js
@@ -30,7 +30,8 @@ const store = createStore(
   rootReducer,
 
   persistedState,
-  compose(applyMiddleware(...middleware)
+  compose(applyMiddleware(...middleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
   )
   );
 
